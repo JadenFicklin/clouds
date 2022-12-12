7bbc20c37f9cd67e75a848e61700502aaa9b5c8f
JadenFicklin <jadendficklin@gmail.com>
2022-12-12 14:07:21 -0700

    styling position for home page video

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,18 @@
 import React from "react";
 import ReactPlayer from "react-player";
-import cloudsVideo from "../assets/clouds.mp4";
 import "../styles/Home.css";
 
 function Home() {
-  const vidClouds = cloudsVideo;
-
   return (
     <>
       <div className="home-outer">
         <ReactPlayer
-          url={vidClouds}
+          url={"assets/clouds.mp4"}
           playing={true}
           muted
           loop={true}
-          width={"100%"}
-          height={"100%"}
+          width="100%"
+          height="100%"
         />
       </div>
     </>
