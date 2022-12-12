0ec608704938ffcb0da0d800aa3e131cd8b1c139
JadenFicklin <jadendficklin@gmail.com>
2022-12-12 14:29:40 -0700

    some styling and button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactPlayer from "react-player";
 import "../styles/Home.css";
 
 function Home() {
+  const [button, setButton] = useState(false);
+  console.log(button);
   return (
     <>
-      <div className="home-outer">
+      <div className="home-button">
+        <div
+          className={button ? "home-text-false" : "home-text"}
+          onClick={() => setButton(!button)}
+        ></div>
+      </div>
+      <div className={button ? "home-outer-false" : "home-outer"}>
         <ReactPlayer
           url={"assets/clouds.mp4"}
           playing={true}
